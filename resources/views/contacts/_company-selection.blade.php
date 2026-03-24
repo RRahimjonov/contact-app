7575cdc113a7344f49f9aa6068a8812b549b61e1
RRahimjonov <temurbekrahimjonov1210@gmail.com>
2026-03-24 13:13:34 +0500

    create va show pagelarini sozladim hamda filter va selectorlar uchun sub viewlar yaratdim

---
+++ resources/views/contacts/_company-selection.blade.php
@@ -0,0 +1,8 @@
+<div class="col">
+    <select class="custom-select">
+        <option value="" selected>All Companies</option>
+        @foreach($companies as $id => $company)
+            <option value="{{ $id }}">{{ $company['name'] }}</option>
+        @endforeach
+    </select>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,18 @@ Route::get('/', function () {
 })->name('welcome');
 
 Route::get('/contacts', function(){
+    $companies = [
+        1 => ['name' => 'Company 1', 'contacts' => 3],
+        2 => ['name' => 'Company 2', 'contacts' => 5],
+    ];
     $contacts = getContacts();
-    return view('contacts.index')->with('contacts', $contacts);
+    return view('contacts.index', compact('contacts', 'companies'));
 })->name('contacts.index');
 
 Route::get('/contacts/{id}', function ($id){
-    return view('contacts.show')->with('id', $id);
+    $contacts = getContacts();
+    $contact = $contacts[$id];
+    return view('contacts.show')->with('contact', $contact);
 })->whereNumber('id')->name('contacts.show');
 
 Route::get('/contacts/create', function (){
