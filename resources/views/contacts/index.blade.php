0c3d69391fa6e804a1019665bc0f859b1bba5fc2
RRahimjonov <temurbekrahimjonov1210@gmail.com>
2026-03-24 22:26:41 +0500

    Contact uchun va empty contact table uchun alohida view ajratib oldim

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -32,22 +32,10 @@
                                 </thead>
                                 <tbody>
                                 @forelse($contacts as $id => $contact)
-                                    <tr @if($loop->odd) class="table-primary" @endif>
-                                        <th scope="row">{{ $loop->index }}</th>
-                                        <td>{{ $contact['name'] }}</td>
-                                        <td>{{ $contact['phone'] }}</td>
-                                        <td>alfred@test.com</td>
-                                        <td>Company one</td>
-                                        <td width="150">
-                                            <a href="{{ route('contacts.show', $id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
-                                            <a href="#" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
-                                            <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
-                                        </td>
-                                    </tr>
+                                    @include('contacts._contact', ['contact' => $contact])
                                 @empty
-                                    <p>Contacts not found!</p>
+                                    @include('contacts._empty')
                                 @endforelse
-
                                 </tbody>
                             </table>
 
